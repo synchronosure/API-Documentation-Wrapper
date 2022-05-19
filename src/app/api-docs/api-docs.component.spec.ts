2bbc10f6d6d52a60e14f9493c12a208dee545286
synchronormacinas <RMacinas@synchronogroup.com>
2022-05-20 03:57:27 +0800

    Add unit test

--- src/app/api-docs/api-docs.component.spec.ts
+++ src/app/api-docs/api-docs.component.spec.ts
@@ -3,23 +3,34 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ApiDocsComponent } from './api-docs.component';
 
 describe('ApiDocsComponent', () => {
-  let component: ApiDocsComponent;
-  let fixture: ComponentFixture<ApiDocsComponent>;
+    let component: ApiDocsComponent;
+    let fixture: ComponentFixture<ApiDocsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ApiDocsComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ApiDocsComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ApiDocsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ApiDocsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('Swagger URL is not empty', () => {
+        expect(component.swaggerURL).toBeTruthy();
+    });
+
+    it('API option is in correct format', () => {
+        component.apiOptions.forEach((prop) => {
+            expect(Object.keys(prop)).toEqual(
+                jasmine.objectContaining(['name', 'url'])
+            );
+        });
+    });
 });
